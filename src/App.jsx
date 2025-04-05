5865676dc26da54acb20077cf6151586656f868c
AngelMVG <verduzcogonzalezangelmanuel3@gmail.com>
2025-04-05 17:17:36 -0600

    Added footer component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Header from './components/Header/Header'
 import Inicio from './pages/Inicio/Inicio'
 import { BrowserRouter, Routes, Route } from 'react-router'
 import './App.css'
+import Footer from './components/Footer/Footer'
 
 function App() {
 
@@ -12,6 +13,7 @@ function App() {
         <Routes>
           <Route index path="/" element={<Inicio />} />
         </Routes>
+        <Footer />
       </BrowserRouter>
     </>
   )
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import './Footer.css';
+
+function Footer() { 
+    return (
+        <footer className='footer'>
+            <p className='footer-text'>© 2025 Esli: Última Misión. Todos los derechos reservados.</p>
+        </footer>
+    )
+}
+
+export default Footer;
