b55f3e77e06a1f4629349faa9e2916e28a322ee9
AngelMVG <verduzcogonzalezangelmanuel3@gmail.com>
2025-04-06 15:36:44 -0600

    Creation of JefesCard component

---
+++ src/components/JefeCard/JefeCard.jsx
@@ -0,0 +1,24 @@
+import './JefeCard.css';
+
+function JefeCard({ jefe }) { 
+    return (
+        <div className='jefe-card'>
+            <div className='jefe-div-main'>
+                <img className='jefe-img' src="" alt={ jefe.name } />
+                <h2 className='jefe-name'>{ jefe.name }</h2>
+            </div>
+            <div className='jefe-div-info'>
+                <h3 className='jefe-subtitle'>Historia</h3>
+                <p className='jefe-description'>{jefe.description}</p>
+                <h3 className='jefe-subtitle'>Ataques</h3>
+                <ul>
+                    {jefe.attacks.map((attack, index) => (
+                        <li key={index} className='jefe-attack'>{attack}</li>
+                    ))}
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+export default JefeCard;
