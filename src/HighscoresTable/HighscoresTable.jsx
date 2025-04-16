6ff1f274c5169bfbc35b3a34fb4604e580e8a8b4
AngelMVG <verduzcogonzalezangelmanuel3@gmail.com>
2025-04-16 16:46:04 -0600

    Implemented call up to the game API to fetch the highscores

--- src/HighscoresTable/HighscoresTable.jsx
+++ src/HighscoresTable/HighscoresTable.jsx
@@ -2,23 +2,23 @@ import './HighscoresTable.css';
 import { Target, Trophy, Calendar, Skull, Zap, Timer } from 'lucide-react';
 import HighscoreRow from '../components/HighscoreRow/HighscoreRow';
 
-function HighscoresTable({jugadores}) {
+function HighscoresTable({highscores}) {
     return (
         <table className='highscores-table'>
             <thead className='highscores-table-head'>
                 <tr className='highscores-table-row'>
                     <th className='highscores-table-header'>#</th>
                     <th className='highscores-table-header'>Gamertag</th>
+                    <th className='highscores-table-header'><div><Trophy></Trophy>Puntos</div></th>
                     <th className='highscores-table-header'><div><Target></Target>Disparos</div></th>
                     <th className='highscores-table-header'><div><Zap></Zap>Enemigos eliminados</div></th>
                     <th className='highscores-table-header'><div><Skull></Skull>Jefes eliminados</div></th>
                     <th className='highscores-table-header'><div><Timer></Timer>Tiempo</div></th>
                     <th className='highscores-table-header'><div><Calendar></Calendar>Fecha</div></th>
-                    <th className='highscores-table-header'><div><Trophy></Trophy>Puntos</div></th>
                 </tr>
             </thead>
             <tbody className='highscores-table-body'>
-                {jugadores.map((jugador) => <HighscoreRow jugador={ jugador } />)}
+                {highscores.map((highscore) => <HighscoreRow key={highscore.gamertag} highscore={ highscore } />)}
             </tbody>
         </table>
     )
--- src/components/HighscoreRow/HighscoreRow.jsx
+++ src/components/HighscoreRow/HighscoreRow.jsx
@@ -1,39 +1,41 @@
 import './HighscoreRow.css';
 import { Trophy } from 'lucide-react';
+import { formatDate } from '../../utils/dateUtils';
+import { secondsToHoursMinutesAndSeconds } from '../../utils/timeUtils';
 
-function HighscoreRow({jugador}) {
+function HighscoreRow({highscore}) {
     return (
         <tr className='highscore-row'>
             <td className='highscore-cell'>
                 <div>
-                    {jugador.rank <= 3 && (
-                        <Trophy className='highscore-icon' color={jugador.rank === 1 ? 'gold' : jugador.rank === 2 ? 'silver' : 'brown'} size={24} />
+                    {highscore.rank <= 3 && (
+                        <Trophy className='highscore-icon' color={highscore.rank === 1 ? 'gold' : highscore.rank === 2 ? 'silver' : 'brown'} size={24} />
                     )}
-                    <span>{jugador.rank}</span>
+                    <span>{highscore.rank}</span>
                 </div>
             </td>
             <td className='highscore-cell'>
-                <span>{jugador.gamertag}</span>
+                <span>{highscore.gamertag}</span>
             </td>
             <td className='highscore-cell'>
-                <span>{jugador.disparos}</span>
+                <span className={highscore.rank <= 3 ? (
+                    highscore.rank === 1 ? 'highscore-gold' : highscore.rank === 2 ? 'highscore-silver' : 'highscore-brown'
+                ) : ''}>{highscore.points}</span>
             </td>
             <td className='highscore-cell'>
-                <span>{jugador.enemigosDestruidos}</span>
+                <span>{highscore.shots}</span>
             </td>
             <td className='highscore-cell'>
-                <span>{jugador.jefesDestruidos}</span>
+                <span>{highscore.destroyedEnemies}</span>
             </td>
             <td className='highscore-cell'>
-                <span>{jugador.tiempoDeJuego}</span>
+                <span>{highscore.destroyedBosses}</span>
             </td>
             <td className='highscore-cell'>
-                <span>{jugador.fecha}</span>
+                <span>{secondsToHoursMinutesAndSeconds(highscore.gameTime)}</span>
             </td>
             <td className='highscore-cell'>
-                <span className={jugador.rank <= 3 ? (
-                    jugador.rank === 1 ? 'highscore-gold' : jugador.rank === 2 ? 'highscore-silver' : 'highscore-brown'
-                ) : ''}>{jugador.puntosTotales}</span>
+                <span>{formatDate(highscore.updatedAt)}</span>
             </td>
         </tr>
     )
