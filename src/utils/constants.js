2c40a109511fccbd22da76f63aaf7d0ce4756a3d
AngelMVG <verduzcogonzalezangelmanuel3@gmail.com>
2025-04-19 19:54:26 -0600

    Updated bosses info

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,10 +1,10 @@
 export const JEFES = [
     {
       id: "lord-chispas",
-      name: "Lord Chispas-Funken",
-      image: "/placeholder.svg?height=400&width=400",
+      name: "Lord Funken",
+      image: "/images/bosses/lord-chispas.png",
       description:
-        "Antes de ser conocido como Lord Chispas, su nombre era Kael Vortan, un brillante ingeniero y piloto en el sistema Zynex. Dedicó su vida a desarrollar tecnología energética que ayudaría a salvar a su planeta de una inminente extinción energética. Pero su investigación fue robada por el Alto Consejo Galáctico, que la utilizó para alimentar armas destructivas. Traicionado y humillado, Kael intentó sabotear el proyecto, pero fue atrapado en una explosión de antimateria que lo fusionó con su propia tecnología. Su cuerpo ahora irradia energía pura. Su mente, consumida por el rencor, se convirtió en una tormenta ambulante. Se unió a la UDC como su general de asalto, prometiendo que la galaxia conocería el verdadero poder de un rayo traicionado.",
+        "Antes de ser conocido como Lord Chispas, su nombre era Kael Vortan, un brillante ingeniero y piloto en el sistema Zynex. Dedicó su vida a desarrollar tecnología energética que ayudaría a salvar a su planeta de una inminente extinción energética. Pero su investigación fue robada por el Alto Consejo Galáctico, que la utilizó para alimentar armas destructivas. Traicionado y humillado, Kael intentó sabotear el proyecto, pero fue atrapado en una explosión de antimateria que lo fusionó con su propia tecnología. Su cuerpo ahora irradia energía pura. Su mente, consumida por el rencor, se convirtió en una tormenta ambulante. Se unió a la Tecnomatrix como su general de asalto, prometiendo que la galaxia conocería el verdadero poder de un rayo traicionado.",
       attacks: [
         "Tormenta de Neutrinos: Dispara rayos de plasma en todas direcciones, electrificando el suelo y dificultando el movimiento.",
         "Flash: Se teletransporta en diferentes direcciones.",
@@ -13,11 +13,11 @@ export const JEFES = [
       ],
     },
     {
-      id: "bubul-vorr",
-      name: "Bubul'Vorr",
-      image: "/placeholder.svg?height=400&width=400",
+      id: "vuv-ulubu",
+      name: "VuV-Ulubu",
+      image: "/images/bosses/vuv-ulubu.png",
       description:
-        "Bubul’Vorr no nació... fue invocado. Durante una expedición arqueológica en el planeta prohibido Y’ghorr, la UDC descubrió una cripta ancestral que contenía un ser sellado fuera del flujo del tiempo. Ignorando las advertencias, los científicos rompieron el sello... y desataron el horror. Bubul’Vorr era una entidad interdimensional, un parásito cósmico que se alimenta de miedo, carne y recuerdos. Infectó a toda la tripulación en minutos y usó sus cuerpos para tomar forma física. Desde entonces, ha viajado con la UDC, siendo su arma biológica más inestable, una aberración que ni ellos pueden controlar completamente.",
+        "VuV-Ulubu no nació... fue invocado. Durante una expedición arqueológica en el planeta prohibido Y'ghorr, la Tecnomatrix descubrió una cripta ancestral que contenía un ser sellado fuera del flujo del tiempo. Ignorando las advertencias, los científicos rompieron el sello... y desataron el horror. VuV-Ubulubu era una entidad interdimensional, un parásito cósmico que se alimenta de miedo, carne y recuerdos. Infectó a toda la tripulación en minutos y usó sus cuerpos para tomar forma física. Desde entonces, ha viajado con la Tecnomatrix, siendo su arma biológica más inestable, una aberración que ni ellos pueden controlar completamente.",
       attacks: [
         "Ola de Corrupción:  Expulsa un líquido viscoso que ralentiza y drena la vida del jugador.",
         "Garras del Vacío: Sus extremidades se alargan y atraviesan el suelo, saliendo en puntos aleatorios para atacar.",
@@ -25,11 +25,11 @@ export const JEFES = [
       ],
     },
     {
-      id: "monika",
-      name: "M.O.N.I.K.A",
-      image: "/placeholder.svg?height=400&width=400",
+      id: "tronica",
+      name: "Tronica",
+      image: "/images/bosses/tronica.png",
       description:
-        "M.O.N.I.K.A. comenzó como un proyecto pacífico: una IA avanzada diseñada para gobernar y proteger colonias automatizadas. Su núcleo estaba basado en la psique de una comandante real, Mónica Hale, reconocida por su liderazgo y eficiencia. Pero durante una guerra interplanetaria, la UDC secuestró el proyecto y lo reprogramó para convertirse en una unidad de control totalitario, sin emociones, sin ética, solo obediencia. La copia digital de la mente de Mónica intentó resistirse... pero fue corrompida por los nuevos protocolos. Ahora, M.O.N.I.K.A. es la máxima autoridad digital de la UDC. Un ser que cree que la galaxia debe ser purgada de caos y error, y que la única forma de lograrlo es erradicando toda forma de pensamiento impredecible… como tú.",
+        "Troncia comenzó como un proyecto pacífico: una IA avanzada diseñada para gobernar y proteger colonias automatizadas. Su núcleo estaba basado en la psique de una comandante real, reconocida por su liderazgo y eficiencia. Pero durante una guerra interplanetaria, la Tecnomatrix secuestró el proyecto y lo reprogramó para convertirse en una unidad de control totalitario, sin emociones, sin ética, solo obediencia. La copia digital de la mente de la comandante intentó resistirse... pero fue corrompida por los nuevos protocolos. Ahora, Tronica es la máxima autoridad digital de la Tecnomatrix. Un ser que cree que la galaxia debe ser purgada de caos y error, y que la única forma de lograrlo es erradicando toda forma de pensamiento impredecible… como tú.",
       attacks: [
         "Rasho laser: Láser que disparan en patrones sincronizados, obligando al jugador a moverse estratégicamente.",
         "Matriz de Códigos Corruptos: Hackea el sistema del jugador, haciendo que sus controles se invierten temporalmente o que sus armas fallen.",
