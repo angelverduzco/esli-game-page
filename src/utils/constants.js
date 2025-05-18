3725619e26fbb43db3f3d337133b2164b704c5b1
AngelMVG <verduzcogonzalezangelmanuel3@gmail.com>
2025-05-18 17:55:30 -0600

    Changed format of characters' images to webp

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,33 +2,33 @@ export const PERSONAJES = [
   {
     id: "tu",
     name: "Tú",
-    image: "/images/characters/tu.png",
+    image: "/images/characters/tu.webp",
     description: "Eres el último sobreviviente del E.S.L.I., un antiguo tripulante marcado por la tragedia y forjado en el exilio. Silencioso, hábil y determinado, tu vida cambió para siempre tras el ataque de la Tecnomatrix. Con una mezcla de dolor, culpa y una voluntad indomable, te conviertes en una fuerza solitaria que atravesará galaxias por una sola misión: rescatar al comandante Esli y vengar a tu familia perdida.",
   },
   {
     id: "esli",
     name: "Esli",
-    image: "/images/characters/esli.png",
+    image: "/images/characters/esli.webp",
     description: "El comandante Esli es un líder noble y determinado, conocido en los rincones más oscuros de la galaxia por su compasión inquebrantable y su valentía frente a la injusticia. Fundador del E.S.L.I. (Ejército Solar de Liberación Interestelar), dedicó su vida a proteger a los oprimidos sin esperar nada a cambio. Astuto, carismático y profundamente humano, Esli es el corazón de una causa que se niega a morir.",
   },
   {
     id: "lord-chispas",
     name: "Lord Funken",
-    image: "/images/characters/lord-funken.png",
+    image: "/images/characters/lord-funken.webp",
     description:
       "Antes de ser conocido como Lord Chispas, su nombre era Kael Vortan, un brillante ingeniero y piloto en el sistema Zynex. Dedicó su vida a desarrollar tecnología energética que ayudaría a salvar a su planeta de una inminente extinción energética. Pero su investigación fue robada por el Alto Consejo Galáctico, que la utilizó para alimentar armas destructivas. Traicionado y humillado, Kael intentó sabotear el proyecto, pero fue atrapado en una explosión de antimateria que lo fusionó con su propia tecnología. Su cuerpo ahora irradia energía pura. Su mente, consumida por el rencor, se convirtió en una tormenta ambulante. Se unió a la Tecnomatrix como su general de asalto, prometiendo que la galaxia conocería el verdadero poder de un rayo traicionado.",
   },
   {
     id: "vuv-ulubu",
     name: "VuV-Ulubu",
-    image: "/images/characters/vuv-ulubu.png",
+    image: "/images/characters/vuv-ulubu.webp",
     description:
       "VuV-Ulubu no nació... fue invocado. Durante una expedición arqueológica en el planeta prohibido Y'ghorr, la Tecnomatrix descubrió una cripta ancestral que contenía un ser sellado fuera del flujo del tiempo. Ignorando las advertencias, los científicos rompieron el sello... y desataron el horror. VuV-Ubulubu era una entidad interdimensional, un parásito cósmico que se alimenta de miedo, carne y recuerdos. Infectó a toda la tripulación en minutos y usó sus cuerpos para tomar forma física. Desde entonces, ha viajado con la Tecnomatrix, siendo su arma biológica más inestable, una aberración que ni ellos pueden controlar completamente.",
   },
   {
     id: "tronica",
     name: "Tronica",
-    image: "/images/characters/tronica.png",
+    image: "/images/characters/tronica.webp",
     description:
       "Troncia comenzó como un proyecto pacífico: una IA avanzada diseñada para gobernar y proteger colonias automatizadas. Su núcleo estaba basado en la psique de una comandante real, reconocida por su liderazgo y eficiencia. Pero durante una guerra interplanetaria, la Tecnomatrix secuestró el proyecto y lo reprogramó para convertirse en una unidad de control totalitario, sin emociones, sin ética, solo obediencia. La copia digital de la mente de la comandante intentó resistirse... pero fue corrompida por los nuevos protocolos. Ahora, Tronica es la máxima autoridad digital de la Tecnomatrix. Un ser que cree que la galaxia debe ser purgada de caos y error, y que la única forma de lograrlo es erradicando toda forma de pensamiento impredecible… como tú.",
   },
