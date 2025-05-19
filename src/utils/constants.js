360794c10a1e9c1b723ce2005322e0f62f5a60ba
AngelMVG <verduzcogonzalezangelmanuel3@gmail.com>
2025-05-18 18:45:24 -0600

    Updated info of some characters

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,7 +1,7 @@
 export const PERSONAJES = [
   {
-    id: "tu",
-    name: "Tú",
+    id: "esli-6",
+    name: "E.S.L.I #6",
     image: "/images/characters/tu.webp",
     description: "Eres el último sobreviviente del E.S.L.I., un antiguo tripulante marcado por la tragedia y forjado en el exilio. Silencioso, hábil y determinado, tu vida cambió para siempre tras el ataque de la Tecnomatrix. Con una mezcla de dolor, culpa y una voluntad indomable, te conviertes en una fuerza solitaria que atravesará galaxias por una sola misión: rescatar al comandante Esli y vengar a tu familia perdida.",
   },
@@ -12,11 +12,11 @@ export const PERSONAJES = [
     description: "El comandante Esli es un líder noble y determinado, conocido en los rincones más oscuros de la galaxia por su compasión inquebrantable y su valentía frente a la injusticia. Fundador del E.S.L.I. (Ejército Solar de Liberación Interestelar), dedicó su vida a proteger a los oprimidos sin esperar nada a cambio. Astuto, carismático y profundamente humano, Esli es el corazón de una causa que se niega a morir.",
   },
   {
-    id: "lord-chispas",
+    id: "lord-funken",
     name: "Lord Funken",
     image: "/images/characters/lord-funken.webp",
     description:
-      "Antes de ser conocido como Lord Chispas, su nombre era Kael Vortan, un brillante ingeniero y piloto en el sistema Zynex. Dedicó su vida a desarrollar tecnología energética que ayudaría a salvar a su planeta de una inminente extinción energética. Pero su investigación fue robada por el Alto Consejo Galáctico, que la utilizó para alimentar armas destructivas. Traicionado y humillado, Kael intentó sabotear el proyecto, pero fue atrapado en una explosión de antimateria que lo fusionó con su propia tecnología. Su cuerpo ahora irradia energía pura. Su mente, consumida por el rencor, se convirtió en una tormenta ambulante. Se unió a la Tecnomatrix como su general de asalto, prometiendo que la galaxia conocería el verdadero poder de un rayo traicionado.",
+      "Antes de ser conocido como Lord Funken, su nombre era Kael Vortan, un brillante ingeniero y piloto en el sistema Zynex. Dedicó su vida a desarrollar tecnología energética que ayudaría a salvar a su planeta de una inminente extinción energética. Pero su investigación fue robada por el Alto Consejo Galáctico, que la utilizó para alimentar armas destructivas. Traicionado y humillado, Kael intentó sabotear el proyecto, pero fue atrapado en una explosión de antimateria que lo fusionó con su propia tecnología. Su cuerpo ahora irradia energía pura. Su mente, consumida por el rencor, se convirtió en una tormenta ambulante. Se unió a la Tecnomatrix como su general de asalto, prometiendo que la galaxia conocería el verdadero poder de un rayo traicionado.",
   },
   {
     id: "vuv-ulubu",
@@ -30,7 +30,7 @@ export const PERSONAJES = [
     name: "Tronica",
     image: "/images/characters/tronica.webp",
     description:
-      "Troncia comenzó como un proyecto pacífico: una IA avanzada diseñada para gobernar y proteger colonias automatizadas. Su núcleo estaba basado en la psique de una comandante real, reconocida por su liderazgo y eficiencia. Pero durante una guerra interplanetaria, la Tecnomatrix secuestró el proyecto y lo reprogramó para convertirse en una unidad de control totalitario, sin emociones, sin ética, solo obediencia. La copia digital de la mente de la comandante intentó resistirse... pero fue corrompida por los nuevos protocolos. Ahora, Tronica es la máxima autoridad digital de la Tecnomatrix. Un ser que cree que la galaxia debe ser purgada de caos y error, y que la única forma de lograrlo es erradicando toda forma de pensamiento impredecible… como tú.",
+      "Tronica comenzó como un proyecto pacífico: una IA avanzada diseñada para gobernar y proteger colonias automatizadas. Su núcleo estaba basado en la psique de una comandante real, reconocida por su liderazgo y eficiencia. Pero durante una guerra interplanetaria, la Tecnomatrix secuestró el proyecto y lo reprogramó para convertirse en una unidad de control totalitario, sin emociones, sin ética, solo obediencia. La copia digital de la mente de la comandante intentó resistirse... pero fue corrompida por los nuevos protocolos. Ahora, Tronica es la máxima autoridad digital de la Tecnomatrix. Un ser que cree que la galaxia debe ser purgada de caos y error, y que la única forma de lograrlo es erradicando toda forma de pensamiento impredecible… como tú.",
   },
 ]
   
