43e4a4774fc49032bdc8536e6be9532d0f3b0712
AngelMVG <verduzcogonzalezangelmanuel3@gmail.com>
2025-07-01 17:41:48 -0600

    Added button to refresh highscores

--- src/pages/Highscores/Highscores.jsx
+++ src/pages/Highscores/Highscores.jsx
@@ -1,5 +1,6 @@
 import './Highscores.css';
 import HighscoresTable from '../../components/HighscoresTable/HighscoresTable';
+import Button from '../../components/Button/Button';
 import { useHighscores } from '../../hooks/useHighscores';
 
 
@@ -16,8 +17,8 @@ function Highscores() {
         <h1 className='highscores-title'>Highscores</h1>
         <p className='highscores-desc'>Los 10 mejores jugadores de Esli: Última Misión</p>
         <section className='highscores-table-container'>
-          {highscores.length === 0 ? <div className='highscores-loading'>Cargando puntuaciones...</div> : <HighscoresTable highscores={highscores}></HighscoresTable>}
-          <button onClick={handleClick}>Actualizar highscores</button>
+          {highscores.length === 0 ? <div className='highscores-loading'>Cargando puntuaciones...</div> : <><HighscoresTable highscores={highscores}></HighscoresTable><Button onClick={handleClick}>Actualizar highscores</Button></>}
+          
         </section>
     </main>
   )
